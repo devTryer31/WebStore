7781266cd87fb73f9567257e14788c1c8aae67c1
Валера Герасимов <valera.gs@bk.ru>
2021-09-19 22:57:02 +0300

    Shop страница добавлена.

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -43,6 +43,9 @@ namespace WebStore.Controllers
 		
 		public IActionResult ProductDetails() =>
 			View();
+		
+		public IActionResult Shop() =>
+			View();
 
 
 		public IActionResult Staff() => //http://localhost:5000/Home/Staff
