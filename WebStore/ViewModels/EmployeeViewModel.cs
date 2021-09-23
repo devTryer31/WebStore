b6a26ea7f83cb107a90697cfc0738463053a8b27
Валера Герасимов <valera.gs@bk.ru>
2021-09-23 18:18:00 +0300

    Добавлен функционал редактирования сотрудников.

---
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Enums;
+
+namespace WebStore.ViewModels
+{
+	public class EmployeeViewModel
+	{
+		public string Name { get; set; }
+
+		public string Surname { get; set; }
+
+		public string Patronymic { get; set; }
+
+		public ushort Age { get; set; }
+
+		public int Id { get; set; }
+
+		public EmployeePositions Position { get; set; }
+
+		public uint Score { get; set; }
+	}
+}
