19a86a82a07660f49b26ffc4c39d9b29273bf740
Валера Герасимов <valera.gs@bk.ru>
2021-09-25 23:03:23 +0300

    Добавлены базовые классы доменных имён.

---
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base
+{
+	public abstract class NamedEntity : INamedEntity
+	{
+		public int Id { get; set; }
+		
+		public string Name { get; set; }
+	}
+}
---
+++ WebStore.Domain/Entities/ProductSection.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+	public class ProductSection : NamedEntity, IOrderedEntity
+	{
+		public int Order { get; set; }
+
+		/// <summary> For tree-like structure. Null if this object is root. </summary>
+		public int? ParentId { get; set; }
+	}
+}
