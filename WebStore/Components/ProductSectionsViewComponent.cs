e41d5945d785ee1bab017cc40576ce78680b447e
Валера Герасимов <valera.gs@bk.ru>
2021-09-26 02:09:21 +0300

    Добавлено отображение категорий из памяти приложения.

---
+++ WebStore/Components/ProductSectionsViewComponent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+	public class ProductSectionsViewComponent : ViewComponent
+	{
+		private readonly IProductsAndBrandsLiteRepository _ProductsAndBrands;
+
+		public ProductSectionsViewComponent( IProductsAndBrandsLiteRepository productsAndBrandsLiteRepository)
+		{
+			_ProductsAndBrands = productsAndBrandsLiteRepository;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var sections = _ProductsAndBrands.GetProductSections();
+
+			var parentSections = sections.Where(s => s.ParentId is null);
+
+			return View(parentSections.Select(ps => new ProductSectionsViewModel
+				{
+					Id = ps.Id,
+					Order = ps.Order,
+					Name = ps.Name,
+					Childs = sections.Where(s => s.ParentId == ps.Id)
+													.Select(cs => new ProductSectionsViewModel
+													{
+														Id = cs.Id,
+														Order = cs.Order,
+														Name = cs.Name,
+														Childs = null
+													})
+													.OrderBy(s => s.Order)
+													.ToList()
+													
+
+				}
+				).OrderBy(s => s.Order)
+			);
+		}
+	}
+}
---
+++ WebStore/ViewModels/ProductSectionsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+	public class ProductSectionsViewModel
+	{
+		public int Id { get; set; }
+
+		public int Order { get; set; }
+
+		public string Name { get; set; }
+
+		public List<ProductSectionsViewModel> Childs { get; set; } = new();
+	}
+}
