ce0549e8767517ffacffcd8c7a5fc7b97107fa27
Валера Герасимов <valera.gs@bk.ru>
2021-09-26 16:43:33 +0300

    Провека работоспособности навигации путём изменения тестовых данных.

--- WebStore/Services/TestDataService.cs
+++ WebStore/Services/TestDataService.cs
@@ -97,7 +97,8 @@ namespace WebStore.Services
             new Product { Id = 9, Name = "Красное китайское платье", Price = 1025, ImgUrl = "product9.jpg", Order = 8, SectionId = 25, BrandId = 1 },
             new Product { Id = 10, Name = "Женские джинсы", Price = 1025, ImgUrl = "product10.jpg", Order = 9, SectionId = 25, BrandId = 3 },
             new Product { Id = 11, Name = "Джинсы женские", Price = 1025, ImgUrl = "product11.jpg", Order = 10, SectionId = 25, BrandId = 3 },
-            new Product { Id = 12, Name = "Летний костюм", Price = 1025, ImgUrl = "product12.jpg", Order = 11, SectionId = 25, BrandId = 3 },
+            new Product { Id = 12, Name = "Летний костюм", Price = 1025, ImgUrl = "product12.jpg", Order = 10, SectionId = 24, BrandId = 3 },
+            new Product { Id = 12, Name = "Летняя сумка Rosch", Price = 1025, ImgUrl = "product12.jpg", Order = 11, SectionId = 24, BrandId = 7 },
         };
    }
 }
