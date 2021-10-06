3f971bc4f9b6b36a8277fbf6973aa63d583f5891
Валера Герасимов <valera.gs@bk.ru>
2021-10-06 16:08:05 +0300

    Первичная инициализация ролей и администратора.

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -48,6 +48,7 @@ namespace WebStore.Controllers
 				return View(_view, model);
 			}
 
+			await _UserManager.AddToRoleAsync(user, Role.Users);
 			await _SignInManager.SignInAsync(user, true);
 			return RedirectToAction("Index", "Home");
 		}
