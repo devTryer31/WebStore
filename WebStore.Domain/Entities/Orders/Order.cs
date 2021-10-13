023fa3cabdb5de16d4f117dba88a2a49f6100193
Валера Герасимов <valera.gs@bk.ru>
2021-10-13 14:25:25 +0300

    Добавлена сущность заказа.

---
+++ WebStore.Domain/Entities/Orders/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Domain.Entities.Orders
+{
+	public class Order : Entity
+	{
+		[Required]
+		public User Сustomer { get; set; }
+
+		[Required]
+		public string Phone { get; set; }
+
+		[Required]
+		public string Address { get; set; }
+
+		public string Description { get; set; }
+
+		[Required]
+		public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+
+		public ICollection<Order> Items { get; set; } = new List<Order>();
+
+	}
+}
