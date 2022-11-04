9ac66842bbf3a259226d94145d641a2d1b9f774d
Валера Герасимов <valera.gs@bk.ru>
2022-11-05 00:14:10 +0300

    404 error test added.

---
+++ Tests/WebStore.UITests/Routing/RoutingTestsBase.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace WebStore.UITests.Routing
+{
+    public class RoutingTestsBase : TestBase
+    {
+        public RoutingTestsBase(BrowserType browser) : base(browser)
+        { }
+
+        [Fact]
+        public void NonExistedEndpointTest()
+        {
+            //arr/act
+            webDriver.Navigate().GoToUrl(webDriver.Url + "/room");
+
+            //assert
+            var imgSrc = webDriver.FindElement(By.XPath("/html/body/div[1]/div[2]/img"))
+                .GetAttribute("src");
+
+            Assert.Equal(@"http://localhost:5000/images/404/404.png", imgSrc);
+        }
+    }
+}
